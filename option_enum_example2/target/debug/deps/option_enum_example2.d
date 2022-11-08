355024a4f387a2db9e3bf6056fde3f64e644eace
suryakantwalunj <suryakantswalunj@gmail.com>
2022-11-08 08:37:21 +0530

    Delete option_enum_example2/target directory

--- option_enum_example2/target/debug/deps/option_enum_example2.d
+++
@@ -1,5 +0,0 @@
-D:\suryakant\rust\rustlang\option_enum_example2\target\debug\deps\option_enum_example2.exe: src\main.rs
-
-D:\suryakant\rust\rustlang\option_enum_example2\target\debug\deps\option_enum_example2.d: src\main.rs
-
-src\main.rs:
--- option_enum_example2/target/debug/option_enum_example2.d
+++
@@ -1,1 +0,0 @@
-D:\suryakant\rust\rustlang\option_enum_example2\target\debug\option_enum_example2.exe: D:\suryakant\rust\rustlang\option_enum_example2\src\main.rs
