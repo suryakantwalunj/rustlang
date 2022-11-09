1987ddcb8a7a6ddef1e9a8313ec90f75f3203b71
Suryakant Walunj <suryakantswalunj@gmail.com>
2022-11-09 07:48:00 +0530

    Added error handling with closure example

--- result_enum_example1/src/main.rs
+++ result_enum_example1/src/main.rs
@@ -1,21 +1,22 @@
-use  std::fs::File;
+#![allow(unused_variables)]
+use std::fs::File;
 use std::io::ErrorKind;
-#[allow(unused_variables )]
+
+
 fn main() {
-    let greeting_file_result = File::open("Hello.txt");
+    let greeting_file_result = File::open("hello.txt");
     
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(error) => match error.kind() {
+        Err(err) => match err.kind() {
             ErrorKind::NotFound => match File::create("hello.txt") {
                 Ok(fc) => fc,
-                Err(e) => panic!("Problem with creating file {:?}",e),
-            },
-            other_error => {
-                panic!("Problem with opening file {:?}",other_error);
+                Err(e) => panic!("Problem on creating a file {:?}",e)
             }
-        }
-        ,
+            other_error => {
+                panic!("Could not open the file. {:?}",other_error);
+            }
+        },
     };
-    //println!("greeting file {}",greeting_file);
+    
 }
