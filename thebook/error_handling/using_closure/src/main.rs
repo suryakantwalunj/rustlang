76b59c6d2e5303737b410a064355fa898e4d8ba6
Suryakant Walunj <suryakantswalunj@gmail.com>
2022-11-09 08:05:53 +0530

    Added new shortcut_to_panic example

--- thebook/error_handling/using_closure/src/main.rs
+++ thebook/error_handling/using_closure/src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+use std::io::ErrorKind;
 fn main() {
-    println!("Hello, world!");
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+            if error.kind() == ErrorKind::NotFound {
+                File::create("hello.txt").unwrap_or_else(|error| {
+                        panic!("problem while creating a file {:?}", error);
+                    })
+            } else {
+                panic!("Problem in opening a file {:?}", error);
+            }
+        });
 }
